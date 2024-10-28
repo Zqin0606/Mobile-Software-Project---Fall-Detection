86aa593c63f66fdd7d22c88f7529922b6311e7c6
Zqin0606 <zhaoyu.qin@student.unimelb.edu.au>
2024-10-28 10:09:58 +0800

    Implement register and login functionality to be tested

--- app/src/main/java/com/example/oldersafe/database/DBDao.java
+++ app/src/main/java/com/example/oldersafe/database/DBDao.java
@@ -40,4 +40,42 @@ public class DBDao {
             db = null;
         }
     }
+    //login functionality
+    public UserBaseInfo Login(String account, String psw, String flag) {
+        Cursor cursor = db.query("userinfo", null, "account=? and psw=? and flag=?", new String[]{account, psw, flag}, null, null, null);
+        UserBaseInfo o = new UserBaseInfo();
+        while (cursor.moveToNext()) {
+            o.infoId = cursor.getString(cursor.getColumnIndex("info_id"));
+            o.userName = cursor.getString(cursor.getColumnIndex("name"));
+            o.phone = cursor.getString(cursor.getColumnIndex("phone"));
+            o.flag = cursor.getString(cursor.getColumnIndex("flag"));
+            o.state = cursor.getString(cursor.getColumnIndex("state"));
+            o.userType = cursor.getString(cursor.getColumnIndex("usertype"));
+        }
+        return o;
+    }
+
+    //Register Functionality
+    public long register(UserBaseInfo o) {
+        ContentValues values = new ContentValues();
+        //Register Contacts
+        values.put("info_id", o.infoId);
+        values.put("name", o.userName);
+        values.put("account", o.account);
+        values.put("psw", o.psw);
+        values.put("phone", o.phone);
+        values.put("usertype", o.userType);
+        values.put("flag", o.flag);
+        values.put("state", o.state);
+        //insert into the data base
+        return db.insert("userinfo", null, values);
+    }
+
+
+
+
+
+
+
+
 }
